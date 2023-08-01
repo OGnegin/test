93908f6b41f55a61fa150a55074d899625490900
OGnegin <vladislausgeorgieff@yandex.ru>
2023-08-02 00:32:35 +0300

    test

---
+++ js/test.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.slider', {
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.next',
+		prevEl: '.prev',
+	},
+	pagination: {
+		el: '.swiper-pagination'
+	}
+	mausewhell: true,
+	keyboard: true
+});
